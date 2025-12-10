9c00447bbe31fbadf0348ffed3caacffee2350fb
Egor Marin <e.marin@enpicom.com>
2025-12-10 15:49:38 +0100

    add both options into binary

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use std::env;
 use std::fs::File;
 
-fn main() {
+fn with_value() {
     let args: Vec<String> = env::args().collect();
 
     let file_path = &args[1];
     println!("{:?}", file_path);
     println!("------------------------------------------------------------");
 
-    let data: serde_pickle::Value = serde_pickle::from_reader(
+    type Active = serde_pickle::Value;
+
+    let data: Active = serde_pickle::from_reader(
         File::open(file_path).unwrap(),
         serde_pickle::de::DeOptions::default(),
     )
     .expect("Could not unwrap data");
     println!("{:?}", data);
+}
+
+fn with_bytes() {
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+    println!("{:?}", file_path);
+    println!("------------------------------------------------------------");
+
+    type Active = Vec<u8>;
+
+    let data: Active = serde_pickle::from_reader(
+        File::open(file_path).unwrap(),
+        serde_pickle::de::DeOptions::default(),
+    )
+    .expect("Could not unwrap data");
+    println!("{:?}", data);
+}
+
+fn main() {
+    with_value();
+    with_bytes();
 }
